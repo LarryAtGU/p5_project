d7d918002b98984faa575af40578ce1e3823927f
Larry Wen <s1834299@staff.ad.griffith.edu.au>
2022-09-23 17:19:00 +1000

    some change

--- p5_projects_a2/q2/sketch.js
+++ p5_projects_a2/q2/sketch.js
@@ -16,14 +16,12 @@ let t = 0;
 let step = 0;
 let trails = [];
 
-function preload()
-{
-  tspFile = loadStrings("TSP_EUC_Problems/berlin52.tsp");
-  tspSolFile = loadStrings("TSP_Solutions/berlin52.sol");
+function preload() {
+  tspFile = loadStrings("TSP_EUC_Problems/t.tsp");
+  tspSolFile = loadStrings("TSP_Solutions/t.sol");
 }
 
-function setup()
-{
+function setup() {
   createCanvas(width, height);
 
   // ------ process tsp header------ //
@@ -31,53 +29,52 @@ function setup()
   // ------ process tsp city ------ //
   cities = loadTSP(tspFile);
   // ------ check file match ------ //
-  if (headers[0][1].trim() == tspSolFile[0])
-    fileMatches = true;
+  console.log(cities);
+  if (headers[0][1].trim() == tspSolFile[0]) fileMatches = true;
 }
 
-function draw()
-{
+function draw() {
   background(255);
   translate(20, 20);
 
   showLoadedTEP();
   showSolution();
-
 }
 
-function loadTSPHeaders(tsp)
-{
+function loadTSPHeaders(tsp) {
   let tspHeaders = [];
-  let node = tspFile.indexOf("NODE_COORD_SECTION")+2;
-  for (let header=0; header<node-1; ++header)
+  let node = tspFile.indexOf("NODE_COORD_SECTION") + 2;
+  for (let header = 0; header < node - 1; ++header)
     tspHeaders.push(tsp[header].trim().split(":"));
 
   return tspHeaders;
 }
 
-function loadTSP(tsp)
-{
+function loadTSP(tsp) {
   let tspCities = [];
-  let node = tsp.indexOf("NODE_COORD_SECTION")+2;
-  for (let city=0; city<tsp.length-node; ++city)
-  {
+  let node = tsp.indexOf("NODE_COORD_SECTION") + 2;
+  for (let city = 0; city < tsp.length - node; ++city) {
     let c = [];
 
-    if (tsp[city+node-1].trim().split(" ")[0] == "EOF")
-      break;
+    if (tsp[city + node - 1].trim().split(" ")[0] == "EOF") break;
 
-    for (fc of tsp[city+node-1].trim().split(" "))
-      fc!='' ? c.push(parseInt(fc)) : undefined;
+    for (fc of tsp[city + node - 1].trim().split(" "))
+      fc != "" ? c.push(parseInt(fc)) : undefined;
 
     tspCities.push(c);
   }
   // console.log(tspCities);
 
-  let minX = 0, minY = 0, maxX = 0, maxY = 0;
-  let minXs =[], minYs = [], maxXs = [], maxYs = [];
+  let minX = 0,
+    minY = 0,
+    maxX = 0,
+    maxY = 0;
+  let minXs = [],
+    minYs = [],
+    maxXs = [],
+    maxYs = [];
 
-  for (let i=0; i<tspCities.length; ++i)
-  {
+  for (let i = 0; i < tspCities.length; ++i) {
     minXs.push(tspCities[i][1]);
     minYs.push(tspCities[i][2]);
     maxXs.push(tspCities[i][1]);
@@ -88,19 +85,16 @@ function loadTSP(tsp)
     maxY = Math.max(...maxYs);
   }
 
-  for (let city=0; city<tspCities.length; ++city)
-  {
-    tspCities[city][1] = map(tspCities[city][1], minX, maxX, 25, width-40);
-    tspCities[city][2] = map(tspCities[city][2], minY, maxY, 25, height-40);
+  for (let city = 0; city < tspCities.length; ++city) {
+    tspCities[city][1] = map(tspCities[city][1], minX, maxX, 25, width - 40);
+    tspCities[city][2] = map(tspCities[city][2], minY, maxY, 25, height - 40);
   }
 
   return tspCities;
 }
 
-function showLoadedTEP()
-{
-  for (city of cities)
-    circle(city[1], city[2], 3);
+function showLoadedTEP() {
+  for (city of cities) circle(city[1], city[2], 3);
 
   textSize(15);
   for (header of headers) {
@@ -110,12 +104,9 @@ function showLoadedTEP()
 }
 
 let returnToFirst = false;
-function showSolution()
-{
-  if (fileMatches)
-  {
-    if (t<1)
-    {
+function showSolution() {
+  if (fileMatches) {
+    if (t < 1) {
       // x = lerp(cities[tspSolFile[step+2]-1][1], cities[tspSolFile[step+3]-1][1], t);
       // y = lerp(cities[tspSolFile[step+2]-1][2], cities[tspSolFile[step+3]-1][2], t);
       // console.log(cities[tspSolFile[2]-1][1]);
@@ -124,26 +115,51 @@ function showSolution()
       //   line(trail[0], trail[1], trail[2], trail[3]);
       // console.log(tspSolFile[cities.length], step, x);
 
-      if (returnToFirst)
-      {
-        x = lerp(cities[tspSolFile[cities.length+1]-1][1], cities[tspSolFile[2]-1][1], t);
-        y = lerp(cities[tspSolFile[cities.length+1]-1][2], cities[tspSolFile[2]-1][2], t);
-        line(cities[tspSolFile[cities.length+1]-1][1], cities[tspSolFile[cities.length+1]-1][2], x, y);
-        console.log(returnToFirst)
+      if (returnToFirst) {
+        x = lerp(
+          cities[tspSolFile[cities.length + 1] - 1][1],
+          cities[tspSolFile[2] - 1][1],
+          t
+        );
+        y = lerp(
+          cities[tspSolFile[cities.length + 1] - 1][2],
+          cities[tspSolFile[2] - 1][2],
+          t
+        );
+        line(
+          cities[tspSolFile[cities.length + 1] - 1][1],
+          cities[tspSolFile[cities.length + 1] - 1][2],
+          x,
+          y
+        );
+        console.log(returnToFirst);
+      } else {
+        x = lerp(
+          cities[tspSolFile[step + 2] - 1][1],
+          cities[tspSolFile[step + 3] - 1][1],
+          t
+        );
+        y = lerp(
+          cities[tspSolFile[step + 2] - 1][2],
+          cities[tspSolFile[step + 3] - 1][2],
+          t
+        );
+        line(
+          cities[tspSolFile[step + 2] - 1][1],
+          cities[tspSolFile[step + 2] - 1][2],
+          x,
+          y
+        );
       }
-      else
-      {
-        x = lerp(cities[tspSolFile[step+2]-1][1], cities[tspSolFile[step+3]-1][1], t);
-        y = lerp(cities[tspSolFile[step+2]-1][2], cities[tspSolFile[step+3]-1][2], t);
-        line(cities[tspSolFile[step+2]-1][1], cities[tspSolFile[step+2]-1][2], x, y);
-
-      }
-
 
       // if (step == cities.length-2 && re == true)
-      if (re == true && trails.length < cities.length-1)
-      {
-        trails.push([cities[tspSolFile[cities.length]-1][1], cities[tspSolFile[cities.length]-1][2], cities[tspSolFile[cities.length+1]-1][1], cities[tspSolFile[cities.length+1]-1][2]]);
+      if (re == true && trails.length < cities.length - 1) {
+        trails.push([
+          cities[tspSolFile[cities.length] - 1][1],
+          cities[tspSolFile[cities.length] - 1][2],
+          cities[tspSolFile[cities.length + 1] - 1][1],
+          cities[tspSolFile[cities.length + 1] - 1][2],
+        ]);
         // post operation
         returnToFirst = true;
         re = false;
@@ -152,31 +168,47 @@ function showSolution()
         //step=0;
       }
 
-
-      t+=0.03*deltaTime/10;
-    }
-    else if (step < cities.length-2)
-    // else if (step < cities.length-2)
-    {
+      t += 0.01 * (deltaTime / 10);
+    } else if (step < cities.length - 2) {
+      // else if (step < cities.length-2)
       if (!repeat) {
-        trails.push([cities[tspSolFile[step+2]-1][1], cities[tspSolFile[step+2]-1][2], x, y]);
+        console.log(cities);
+        console.log(step, tspSolFile);
+        trails.push([
+          cities[tspSolFile[step + 2] - 1][1],
+          cities[tspSolFile[step + 2] - 1][2],
+          cities[tspSolFile[step + 3] - 1][1],
+          cities[tspSolFile[step + 3] - 1][2],
+        ]);
       }
 
       ++step;
-      t=0;
-    }
-    else
-    {
+      t = 0;
+    } else {
+      if (!repeat) {
+        trails.push([
+          cities[tspSolFile[step + 3] - 1][1],
+          cities[tspSolFile[step + 3] - 1][2],
+          cities[tspSolFile[2] - 1][1],
+          cities[tspSolFile[2] - 1][2],
+        ]);
+        trails.push([
+          cities[tspSolFile[step + 2] - 1][1],
+          cities[tspSolFile[step + 2] - 1][2],
+          cities[tspSolFile[step + 3] - 1][1],
+          cities[tspSolFile[step + 3] - 1][2],
+        ]);
+      }
       repeat = true;
       re = true;
       t = 0;
       returnToFirst = false;
       step = 0;
-      print("return")
+      print("return");
       // step = 0;
       // o = false;
     }
-//console.log(cities[tspSolFile[cities.length]-1][0]);
+    //console.log(cities[tspSolFile[cities.length]-1][0]);
 
     // if (returnToFirst)
     // {
@@ -187,19 +219,21 @@ function showSolution()
     // line(cities[tspSolFile[cities.length+1]-1][1], cities[tspSolFile[cities.length+1]-1][2], x, y);
     // }
 
-    line(cities[tspSolFile[step+2]-1][1], cities[tspSolFile[step+2]-1][2], x, y);
+    line(
+      cities[tspSolFile[step + 2] - 1][1],
+      cities[tspSolFile[step + 2] - 1][2],
+      x,
+      y
+    );
 
-    for (trail of trails)
-      line(trail[0], trail[1], trail[2], trail[3]);
+    for (trail of trails) line(trail[0], trail[1], trail[2], trail[3]);
 
     push();
-    fill(255,0,0);
+    fill(255, 0, 0);
     circle(x, y, 6);
     pop();
-  }
-  else
-  {
+  } else {
     textSize(30);
-    text("Please check file match!!", width/2-200, height/2-20);
+    text("Please check file match!!", width / 2 - 200, height / 2 - 20);
   }
 }
